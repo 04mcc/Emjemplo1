4ee56d94e394663be0c4e1ef53c2a72d74bbd8d3
marina castro <marina castro@LAPTOP-R16J7I4O>
2023-05-02 11:15:08 +0200

    Primera subida

---
+++ EjemplosGit/src/A.java
@@ -0,0 +1,11 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");
+
+	}
+
+}
