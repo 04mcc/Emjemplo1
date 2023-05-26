fb3dfc87cdb3e8856302bf6f89495f1935a9f20b
marina castro <marina castro@LAPTOP-R16J7I4O>
2023-05-26 21:18:08 +0200

    Empezamos a crear el metodo vender vehiculo

---
+++ P810_Grupo4/P810_Grupo4-master/P810_grupo4/src/p810_grupo4/Concesionario.java
@@ -0,0 +1,73 @@
+package p810_grupo4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Concesionario {
+	private List<Vehiculo> inventario;
+    
+	public Concesionario() {
+        inventario = new ArrayList<>();
+    }
+
+	 public void agregarVehiculo(Vehiculo vehiculo) {
+	        inventario.add(vehiculo);
+	    }
+	  public void mostrarVehiculosDisponibles() {
+	        List<Vehiculo> vehiculosDisponibles = new ArrayList<>(inventario);
+	        Collections.sort(vehiculosDisponibles, Comparator.comparingDouble(Vehiculo::getPrecio));
+	        for (Vehiculo vehiculo : vehiculosDisponibles) {
+	            System.out.println(vehiculo.toString());
+	        }
+	        
+	    }
+	  public List<Vehiculo> buscarVehiculos(String marca, String modelo, int año) {
+	        List<Vehiculo> vehiculosEncontrados = new ArrayList<>();
+	        for (Vehiculo vehiculo : inventario) {
+	            if ((marca == null || vehiculo.getMarca().equals(marca))
+	                    && (modelo == null || vehiculo.getModelo().equals(modelo))
+	                    && (año == 0 || vehiculo.getAño() == año)) {
+	                vehiculosEncontrados.add(vehiculo);
+	            }
+	        }
+	        return vehiculosEncontrados;
+	    }
+	  public Vehiculo obtenerVehiculoMasCaro() {
+	        return Collections.max(inventario, Comparator.comparingDouble(Vehiculo::getPrecio));
+	    }
+	  
+	  public Vehiculo obtenerVehiculoMasBarato() {
+	        return Collections.min(inventario, Comparator.comparingDouble(Vehiculo::getPrecio));
+	    }
+	  
+	  
+	  
+	  
+	  private List<Vehiculo> inventario;
+	    
+	  public Concesionario() {
+	          inventario = new ArrayList<>();
+	      }
+
+	      public void agregarVehiculo(Vehiculo vehiculo) {
+	          inventario.add(vehiculo);
+	      }
+	      
+	  
+}
+	  
+
+
+
+
+
+
+
+
+
+
+
+
+
